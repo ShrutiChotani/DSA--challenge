5aad1fa8ee0ca6fe091405c5430fbc5149484463
Shruti_Chotani <shrutichotani21@gmail.com>
2024-07-14 19:07:46 +0530

    second commit

--- GREEDY/134GasStatio.cpp
+++ GREEDY/134GasStatio.cpp
@@ -8,7 +8,7 @@ public:
         int remain = accumulate(gas.begin(), gas.end(), 0) - accumulate(cost.begin(), cost.end(), 0);
         if (remain < 0) return -1;
 
-        int start = 0;
+        int start= 0;
         int currRemain = 0;
         for (int i = 0; i < n; ++i) {
             currRemain += gas[i] - cost[i];
@@ -22,7 +22,7 @@ public:
     }
 };
 
-// Example usage
+
 int main() {
     Solution solution;
     vector<int> gas = {1, 2, 3, 4, 5};
